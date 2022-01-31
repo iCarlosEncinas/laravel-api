6a75e3b3551eebbd3cc0e8057e51ce6cfd2c9cfd
Moises <davidencinas@Davids-MacBook-Air.local>
2022-01-31 16:16:40 -0700

    uwu

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/noticias', 'NoticiasController@index')->name('noticias.index');
+Route::get('/noticias/create', 'NoticiasController@create')->name('noticias.create');
+Route::post('/noticias/store', 'NoticiasController@store')->name('noticias.store');
